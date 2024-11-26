ada2e600f4b9a81c73a8a12caf688c4219e8eead
KismatMudvari <kismatm31@gmail.com>
2024-11-26 20:44:09 +0545

    day 3

---
+++ Calculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+    public class Calculator {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner (System.in);
+            System.out.println("Enter the value of a:");
+            int a= sc.nextInt();
+            System.out.println("Enter the value of b:");
+            int b = sc.nextInt();
+
+            // let user choose their desired operation
+             System.out.println("Choose your operation");
+             System.out.println("1.Add(+)");
+             System.out.println("2.subtrcation(-)");
+             System.out.println("3.Multiply(*)");
+             System.out.println("4.Divide(/)");
+             System.out.println("5.Modulo(%)");
+                int choice=sc.nextInt();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("The result of addition is:"+(a+b));
+                        break;
+
+                    case 2:
+                        System.out.println("The result of subtrcation is :"+ (a-b));
+                        break;
+
+                    case 3:
+                        System.out.println("The result of Multiply is:"+ (a*b));
+                        break;
+
+                    case 4:
+                        if (b != 0) {
+                            System.out.println("The result of division is: " + (a / b));
+                        } else {
+                            System.out.println("Division by zero is not allowed.");
+                        }
+                        break;
+
+                    case 5:
+                        if (b != 0) {
+                            System.out.println("The result of modulo is: " + (a % b));
+                        } else {
+                            System.out.println("Modulo by zero is not allowed.");
+                        }
+                        break;
+
+                    default:
+                    System.out.println("Please enter the number from from 1 to 5 only.");
+                        break;
+                }
+                sc.close();
+            }
+}
