9b61037b9b27f0ed2acede3bd3ed348fa646fd07
KismatMudvari <kismatm31@gmail.com>
2024-12-02 15:29:11 +0545

    day 5

---
+++ patternssss.java
@@ -0,0 +1,11 @@
+public class patternssss {
+    public static void main(String[] args) {
+        int n =10;
+        for(int i = 1 ; i<11; i ++){
+            for(int j =1 ; j <= i; j ++){
+                System.out.print("x");
+            }
+            System.out.println();
+        }
+    }
+}
