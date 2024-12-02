e5298e399474de3c267d3c59cbd03cb040a41151
KismatMudvari <kismatm31@gmail.com>
2024-12-02 14:33:58 +0545

    day 5

--- pattern3.java
+++ pattern3.java
@@ -1,7 +1,7 @@
 
 public class pattern3 {
     public static void main(String[] args) {
-        for(int i = 1 ; i<=4;i++){
+        for(int i = 1 ; i<=4 ;i++){
             for (int j=1; j<=5 ; j ++){
                 if (i ==1 || j== 1|| i ==4 || j==5){
                     System.out.print("*");
@@ -9,8 +9,7 @@ public class pattern3 {
                 else{
                     System.out.print(" ");
             }
-        }
-        
+            }
             System.out.println();
         }
         }
