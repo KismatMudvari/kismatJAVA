68d718fe2d8ddc35ef1cce35e47ac5e78909e519
KismatMudvari <kismatm31@gmail.com>
2024-12-02 15:45:25 +0545

    day 5

--- numberspatternns.java
+++ numberspatternns.java
@@ -2,8 +2,8 @@ public class numberspatternns {
     public static void main(String[] args) {
         int n = 5;
         for(int i=1; i<=n; i ++){
-            for( int j = 1; j<= i; j ++){
-                System.out.print(j+" ");
+            for( int j = 1; j<= n-i+1; j ++){
+                System.out.print(j);
             }
             System.out.println();
         }
