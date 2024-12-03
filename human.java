98a91ef3548251b92ed159fdb5fde551d6ffa69f
KismatMudvari <kismatm31@gmail.com>
2024-12-03 20:41:28 +0545

    day 6

---
+++ human.java
@@ -0,0 +1,8 @@
+public class human {
+    public static void main(String[] args) {
+        System.out.println("      O\n" + //
+                        "     /|\\\n" + //
+                        "     / \\");
+    }
+    
+}
