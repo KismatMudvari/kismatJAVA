e6e91cda866ff83790117a7045e8b2c472ec6ebc
KismatMudvari <kismatm31@gmail.com>
2024-12-04 17:52:06 +0545

    day 7

---
+++ FactoryDemo.java
@@ -0,0 +1,36 @@
+interface Shape {
+    void draw();
+}
+
+class Circle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
+
+class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
+
+class ShapeFactory {
+    public static Shape getShape(String shapeType) {
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        }
+        return null;
+    }
+}
+
+public class FactoryDemo {
+    public static void main(String[] args) {
+        Shape shape1 = ShapeFactory.getShape("CIRCLE");
+        shape1.draw();
+
+        Shape shape2 = ShapeFactory.getShape("RECTANGLE");
+        shape2.draw();
+    }
+}
