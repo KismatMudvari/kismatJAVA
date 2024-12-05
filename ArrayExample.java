d5bff4ede18761af5d33e7667291beb8ebb9cebe
KismatMudvari <kismatm31@gmail.com>
2024-12-05 21:23:11 +0545

    day 8

---
+++ ArrayExample.java
@@ -0,0 +1,13 @@
+public public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+        System.out.println("Array elements:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
+ {
+    
+}
