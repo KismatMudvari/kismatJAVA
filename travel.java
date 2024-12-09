18f5c9e01dfec32438775b1b3e6d5100f3f54ce4
KismatMudvari <kismatm31@gmail.com>
2024-12-09 22:14:43 +0545

    day 12

---
+++ travel.java
@@ -0,0 +1,5 @@
+public class travel {
+    public static void main(String[] args) {
+        System.out.println("Hello  GUYUS I`m travelling right now and I`m coding but just for fun and Alsp iI need to maintain mmy profile and streak on my github .Literally guys Its freeezing cold outsideas it is winter season already 7:04pm dark as fk.");
+    }
+}
