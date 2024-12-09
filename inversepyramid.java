c6c1636afc4e11dd95b7596822f49721deb488a0
KismatMudvari <kismatm31@gmail.com>
2024-12-09 22:17:30 +0545

    day 12

--- inversepyramid.java
+++ inversepyramid.java
@@ -1,9 +1,9 @@
 public class inversepyramid {
     public static void main(String[] args) {
-        int n =4;
+        int n =7;
         for(int i =n ; i>=1; i --){
-            for(int j = 1; j <= i; j ++){
-                System.out.print("x");
+            for(int j = 1; j <= 2*i-n; j ++){
+                System.out.print("*");
             }
         System.out.println();
         }
