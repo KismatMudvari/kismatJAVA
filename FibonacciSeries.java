e7e9ed48c85bf461a2220a52f49dd8a13a494041
KismatMudvari <kismatm31@gmail.com>
2024-12-10 20:20:14 +0545

    day 13

---
+++ FibonacciSeries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms for the Fibonacci series:");
+        int terms = scanner.nextInt();
+
+        if (terms <= 0) {
+            System.out.println("Please enter a positive number.");
+        } else {
+            System.out.println("Fibonacci Series:");
+            int a = 0, b = 1;
+            for (int i = 1; i <= terms; i++) {
+                System.out.print(a + " ");
+                int nextTerm = a + b;
+                a = b;
+                b = nextTerm;
+            }
+        }
+    }
+}
