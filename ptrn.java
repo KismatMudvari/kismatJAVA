8ab3cd3a68df4322e732622ef487b8762519dfaf
KismatMudvari <kismatm31@gmail.com>
2024-12-17 17:32:05 +0545

    day 18

--- ptrn.java
+++ ptrn.java
@@ -4,8 +4,8 @@ public class ptrn {
         for(int i=1;i<=n;i++)
         {
             for(int j =1; j<=i; j++){
-                int sum=i+j;
-                if(sum % 2==0){
+              //  int sum=i+j;
+                if((i+j) % 2==0){
                     System.out.print("1 ");
                 }
                 else{
