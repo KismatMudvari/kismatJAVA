81665ecbe4260ee907b8f3cef5653dbafc928030
KismatMudvari <kismatm31@gmail.com>
2024-12-18 18:41:45 +0545

    day 19

---
+++ patternREVISION/pyramid.java
@@ -0,0 +1,18 @@
+
+public class pyramid {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows for the pyramid
+
+        for (int i = 1; i <= n; i++) { // Outer loop for rows
+            for (int j = 1; j <= n - i; j++) { // Loop for leading spaces
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) { // Loop for numbers
+                System.out.print(i + " ");
+            }
+
+            System.out.println(); // Move to the next line
+        }
+    }
+}
