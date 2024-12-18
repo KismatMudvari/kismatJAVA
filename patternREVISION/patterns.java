5f9a9b74d3355d0169d1e3bb27b141718d3107ff
KismatMudvari <kismatm31@gmail.com>
2024-12-18 16:03:12 +0545

    day 19

---
+++ patternREVISION/patterns.java
@@ -0,0 +1,15 @@
+package patternREVISION;
+
+public class patterns {
+    public static void main(String[] args) {
+        int n=5;
+        int w=4;
+        for(int i=1; i<=w; i++){
+            for(int j=1;j<=n ;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
