18e821855bf12c6d4e7b214d3c574b1ef6257cfd
KismatMudvari <kismatm31@gmail.com>
2024-12-22 17:04:01 +0545

    day 20

---
+++ DIamond.java
@@ -0,0 +1,37 @@
+public class DIamond {
+    public static void main(String[] args) {
+        int n = 5; // Height of the diamond
+
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+
+            // Move to the next line
+            System.out.println();
+        }
+    }
+}
