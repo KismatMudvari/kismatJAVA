9668822ab5d719f8f70bcfbc0c80a94af6131d8d
KismatMudvari <kismatm31@gmail.com>
2024-12-23 20:22:56 +0545

    day 21

---
+++ streak.java
@@ -0,0 +1,5 @@
+public class streak {
+    public static void main(String[] args) {
+        System.out.println("hello guys I am trying to code daily and push to my git hub nevertheeless I am born in this kind of shit country which doesn`t provide you proper electricity .sorry I have to maintain my streak like this.. I am sorry ");
+    }
+}
