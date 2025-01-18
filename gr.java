f177f0167c12d64c006d078d0e79073b46a818db
KismatMudvari <kismatm31@gmail.com>
2025-01-18 23:02:35 +0545

    day 32

---
+++ gr.java
@@ -0,0 +1,19 @@
+public class gr {
+
+    // Function to calculate GCD using the Euclidean algorithm
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a); // Ensure the GCD is always positive
+    }
+
+    public static void main(String[] args) {
+        int num1 = 56;
+        int num2 = 98;
+
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is " + gcd(num1, num2));
+    }
+}
