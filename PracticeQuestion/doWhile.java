1fb2a130ffe7cf5771accc85d01d2c58d7e0e736
KismatMudvari <kismatm31@gmail.com>
2025-01-20 20:33:58 +0545

    day 34

---
+++ PracticeQuestion/doWhile.java
@@ -0,0 +1,11 @@
+package PracticeQuestion;
+
+public class doWhile {
+    public static void main(String[] args) {
+        int a = 999999999;
+        do {
+            System.out.println(a);
+            a++;
+        } while (a > 999999999);
+    }
+}
