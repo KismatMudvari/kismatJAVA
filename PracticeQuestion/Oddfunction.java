e09124c9faa9b8c0d104a53ea02acd4de37607b9
KismatMudvari <kismatm31@gmail.com>
2025-01-20 19:14:20 +0545

    day 34

--- PracticeQuestion/Oddfunction.java
+++ PracticeQuestion/Oddfunction.java
@@ -4,5 +4,16 @@ package PracticeQuestion;
 import java.util.*;
 
 public class Oddfunction {
+    public static int oddsum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i += 2)
+            sum += i;
+        return sum;
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("the sum of all odd numbers from 1 to n is " + oddsum(n));
+    }
 }
