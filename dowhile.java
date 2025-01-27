809602a32dceceb19ff5db441ee562ddf2bd81b5
KismatMudvari <kismatm31@gmail.com>
2025-01-27 21:53:56 +0545

    day 36

---
+++ dowhile.java
@@ -0,0 +1,12 @@
+//Write an infinite loop using do while condition.
+
+public class dowhile {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.println(a);
+            a++;
+        } while (true);
+    }
+
+}
