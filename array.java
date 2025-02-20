fbd71d821c6ed576dc7205cac0105a5573f78a81
KismatMudvari <kismatm31@gmail.com>
2025-02-20 21:27:27 +0545

    day 38

--- array.java
+++ array.java
@@ -1,10 +1,12 @@
- public class array {
+public class array { // Renamed class
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        
-        System.out.println("Array elements:");
-        for (int num : numbers) {
-            System.out.println(num);
-        }
+        int[] marks = new int[3]; // Space between `int []` is fine
+        marks[0] = 70;
+        marks[1] = 75;
+        marks[2] = 67;
+
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
     }
 }
