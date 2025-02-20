036f000d215a5232309855ebb0fdb318845a9e10
KismatMudvari <kismatm31@gmail.com>
2025-02-20 21:42:57 +0545

    day 38

--- array.java
+++ array.java
@@ -1,9 +1,9 @@
-public class array { // Renamed class
+public class array {
     public static void main(String[] args) {
-        int[] marks = new int[3]; // Space between `int []` is fine
-        marks[0] = 70;
-        marks[1] = 75;
-        marks[2] = 67;
+        int[] marks = new int[3];
+        marks[0] = 70;// math
+        marks[1] = 75;// science
+        marks[2] = 67;// English
 
         System.out.println(marks[0]);
         System.out.println(marks[1]);
