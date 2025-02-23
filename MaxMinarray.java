3b0dc6ffc5c885b9799abba40332e3742ed6ca40
KismatMudvari <kismatm31@gmail.com>
2025-02-23 21:57:55 +0545

    day 39

---
+++ MaxMinarray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class MaxMinarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int number[] = new int[size];
+        // input
+        for (int i = 0; i < size; i++) {
+            number[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] < min) {
+                min = number[i];
+            }
+            if (number[i] > max) {
+                max = number[i];
+            }
+        }
+
+        System.out.println("Largest is " + max);
+        System.out.println("Smallest  is " + min);
+    }
+}
