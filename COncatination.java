66124a807f427ad2037300236d7d12f3dd456773
KismatMudvari <kismatm31@gmail.com>
2025-03-06 20:10:16 +0545

    day 42

---
+++ COncatination.java
@@ -0,0 +1,9 @@
+public class COncatination {
+    public static void main(String[] args) {
+        String name = "Ram";
+        String Full_Name = "Chandra";
+        // COncationation
+        String FNAME = name + Full_Name;
+        System.out.println(FNAME);
+    }
+}
