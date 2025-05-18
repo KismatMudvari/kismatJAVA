f908e8eec2d8ed0fc46e2d8e50f67d921b9e63ed
KismatMudvari <kismatm31@gmail.com>
2025-05-18 21:50:02 +0545

    new day 1

---
+++ Revise.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Revise {
+    public static void main(String[] args) {
+        int a = 4, b = 5;
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
+                if (i == 1 || j == 1 || i == 4 || j == 5) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print("");
+        }
+    }
+}
---
+++ neww.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class neww {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
