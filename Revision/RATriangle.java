616835ae2a01e5204a4e098c1f9d1100f272ca8c
KismatMudvari <kismatm31@gmail.com>
2025-05-19 22:22:35 +0545

    revision day 2

---
+++ Revision/RATriangle.java
@@ -0,0 +1,13 @@
+package Revision;
+
+public class RATriangle {
+    public static void main(String[] args) {
+        int a = 5, b = 5;
+        for (int i = 0; i <= a; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("x");
+            }
+            System.out.println();
+        }
+    }
+}
